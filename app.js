1307e62280ee6b4f6d0a31e3054bb35b9653143d
Timo Strauch <timo.strauch@googlemail.com>
2016-04-06 23:22:23 +0200

    test setup up and running

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if (app.get('env') === 'development') {
   app.use(express.static(path.join(__dirname, '../client/app')));
   //CORS middleware for local testing
   var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
